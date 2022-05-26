67ea6ec52e63250c04239f73c1f7c7e48abb3618
Oscar Farfan <oscarfarfan92@gmail.com>
2022-05-25 22:59:56 -0500

    Se agregan los métodos de la clase Calculator

--- src/main/java/com/sofka/domain/Calculator.java
+++ src/main/java/com/sofka/domain/Calculator.java
@@ -1,5 +1,16 @@
 package com.sofka.domain;
 
 public class Calculator {
-
+    public double sumar(double numberOne, double numerTwo){
+        return numberOne + numerTwo;
+    }
+    public double resta(double numberOne, double numerTwo){
+        return numberOne - numerTwo;
+    }
+    public double multiplicar(double numberOne, double numerTwo){
+        return numberOne * numerTwo;
+    }
+    public double division(double numberOne, double numerTwo){
+        return numberOne / numerTwo;
+    }
 }
